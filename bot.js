630293e540f4081879e66dedf50fece6628b41d7
Douglas Wender <douglas@servofiel.com.br>
2020-04-14 16:20:42 -0400

    fix

--- bot.js
+++ bot.js
@@ -11,68 +11,62 @@ const reader = require('./src/readPage');
 
 require('dotenv/config');
 
-module.exports = {
-    login() {
-        client.login(process.env.TOKEN);
-    },
-    ready() {
-        client.on('ready', async () => {
-            //668808435579486220
-            var testChannel;
 
-            client.channels.fetch('668808435579486220').then(channel => {
-                testChannel = channel;
-            });
+client.login(process.env.TOKEN);
 
-            // Set the client user's activity
-            client.user.setActivity('$help', { type: 'LISTENING' })
-                .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-                .catch(console.error);
+client.on('ready', async () => {
+    //668808435579486220
+    var testChannel;
 
-            data = await reader.getUrl(patchUrl);
+    client.channels.fetch('668808435579486220').then(channel => {
+        testChannel = channel;
+    });
 
-            if (latestUrl !== data[0]) {
-                console.log('atualizando patch notes');
-                latestUrl = data[0];
-                const patch = data[0].split('.');
-                console.log(patch);
-                testChannel.send(`https://br.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-${patch[0]}-${patch[1]}/`);
+    // Set the client user's activity
+    client.user.setActivity('$help', { type: 'LISTENING' })
+        .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .catch(console.error);
 
-            }
+    data = await reader.getUrl(patchUrl);
 
-
-            setInterval(async () => {
-
-                data = await reader.getUrl(patchUrl);
-
-                console.log(data);
-
-                if (latestUrl !== data[0]) {
-                    console.log('atualizando patch notes');
-                    latestUrl = data[0];
-                    const patch = data[0].split('.');
-                    console.log(patch);
-                    testChannel.send(`https://br.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-${patch[0]}-${patch[1]}/`);
-
-                }
-
-
-            }, 3600000);
-
-        });
-    },
-    message() {
-        client.on("message", (message) => { // EventEmitter
-            if (message.content == "!ping") { // Check if message is "!ping"
-                message.channel.send("Pinging ...") // Placeholder for pinging ... 
-                    .then((msg) => { // Resolve promise
-                        msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
-                    });
-            }
-        });
+    if (latestUrl !== data[0]) {
+        console.log('atualizando patch notes');
+        latestUrl = data[0];
+        const patch = data[0].split('.');
+        console.log(patch);
+        testChannel.send(`https://br.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-${patch[0]}-${patch[1]}/`);
 
     }
-}
+
+
+    setInterval(async () => {
+
+        data = await reader.getUrl(patchUrl);
+
+        console.log(data);
+
+        if (latestUrl !== data[0]) {
+            console.log('atualizando patch notes');
+            latestUrl = data[0];
+            const patch = data[0].split('.');
+            console.log(patch);
+            testChannel.send(`https://br.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-${patch[0]}-${patch[1]}/`);
+
+        }
+
+
+    }, 3600000);
+
+});
+
+client.on("message", (message) => { // EventEmitter
+    if (message.content == "!ping") { // Check if message is "!ping"
+        message.channel.send("Pinging ...") // Placeholder for pinging ... 
+            .then((msg) => { // Resolve promise
+                msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
+            });
+    }
+});
 
 
 
